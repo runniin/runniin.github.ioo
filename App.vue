5d2b3d51371e14dddb5d7cd0aef301bb5f3cb5ca
lanfeihong <481243231@qq.com>
2022-06-13 13:12:40 +0800

    web

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import "./assets/css/reset.css";
+export default {};
+
+console.log('分类的数据请求');
+</script>
+
+<style>
+</style>
